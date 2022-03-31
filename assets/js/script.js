f25a428dce46384e62a3803cf59a6f77e7872aed
edavis56 <92826817+edavis56@users.noreply.github.com>
2022-03-30 21:03:52 -0400

    update to htmle file interest section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 const aboutMe = document.querySelector('.aboutMe');
 const work = document.querySelector('.work'); 
 const aboutMe = document.querySelector('.interest');
+const tree = document.querySelector('.tree');
+const chiefs = document.querySelector('.chiefs');
+const movie = document.querySelector('.movie');
+
+button.addEventListener("click", function(){
